7322dc0371074f78ae30e20abc501384b3fe4f66
cristianMD1 <74798889+cristianMD1@users.noreply.github.com>
2021-03-03 18:16:32 -0600

    Update index.js

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ setInterval(() => {
 
 let deferredPrompt;
 const addBtn = document.querySelector('.add-button');
-addBtn.style.display = 'none';
+addBtn.style.display = 'visibility';
 
 window.addEventListener('beforeinstallprompt', (e) => {
   // Prevent Chrome 67 and earlier from automatically showing the prompt
